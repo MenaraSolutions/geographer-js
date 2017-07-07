db620f791a70bbff57a93f0475d5cc23120d1fd7
dusterio <denis@mysenko.com>
2017-07-07 18:04:47 +1000

    r Tests

--- test/earth.test.js
+++ test/earth.test.js
@@ -1,6 +1,5 @@
 var Earth = require('../lib/index')
 
 test('first test', () => {
-  console.log(Earth().getCountries())
-  expect(5 + 5).toBe(10);
+  expect(Earth().getCountries().length).toBe(251);
 });
