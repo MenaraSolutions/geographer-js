a4e6400c98f3c9af4e5579784569adc4b59b50e7
dusterio <denis@mysenko.com>
2017-07-10 17:33:25 +1000

    + Earth methods

--- test/earth.test.js
+++ test/earth.test.js
@@ -1,5 +1,19 @@
 var Earth = require('../lib/index')
 
-test('first test', () => {
+test('All countries are available in the Earth', () => {
   expect(Earth().getCountries().length).toBe(251);
+});
+
+test('Earth can choose short names', () => {
+  expect(Earth().useShortNames()).toBe(undefined);
+});
+
+test('Earth can filter itself by continents', () => {
+  expect(Earth().getAfrica().length).toBeGreaterThan(0);
+  expect(Earth().getEurope().length).toBeGreaterThan(0);
+  expect(Earth().getNorthAmerica().length).toBeGreaterThan(0);
+  expect(Earth().getSouthAmerica().length).toBeGreaterThan(0);
+  expect(Earth().getAsia().length).toBeGreaterThan(0);
+  expect(Earth().getOceania().length).toBeGreaterThan(0);
+  expect(Earth().withoutMicro().length).toBeGreaterThan(0);
 });
